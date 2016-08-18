052451d908f3c2a35b46f73c238f87e1f24717d3
Hanhan <HanhanTalk@hotmail.com>
2016-08-19 01:53:01 +0800

    add some function

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,45 @@ $(function(){
     var app = new Vue({
       el:'#mail-App',
       data:{
-        AllItem:allItem
+        AllItem:allItem,
+        email:{},
+        show:true,
+        status:'Unread',
+        index:0
       },
       methods:{
         mailOnClick:function(){
 
         },
-        onSwipeLeft:function(){
+        onSwipeLeft:function(event,item,index){
+          item.status = 'read';
+          this.email = item;
+          this.index = index;
+          this.show = false;
+          $(".mail-item").removeClass("hidden");
 
+        },
+        onBack:function(){
+          this.show = true;
+          $(".mail-item").addClass("hidden");
+
+        },
+        getUp:function(){
+          var id = this.index-1;
+          this.forEachData(id);
+          this.index--;
+        },
+        getDown:function() {
+          var id = this.index+1;
+          this.forEachData(id);
+          this.index++;
+        },
+        forEachData:function(id){
+          this.AllItem.forEach(function(element,index){
+            if(index == id){
+              this.email = element;
+            }
+          }.bind(this))
         }
       }
     })
